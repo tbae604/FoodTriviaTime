5076d565438508397701462e1ef184a8b033373b
tkbaylis <tkbaylis@users.noreply.github.com>
2014-05-10 22:44:39 -0700

    edited questions csv; changed from one to two listener classes

--- src/FoodTriviaTime/CorrectListener.java
+++ src/FoodTriviaTime/CorrectListener.java
@@ -3,38 +3,36 @@ package FoodTriviaTime;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-
-public class Listener implements MouseListener {
+public class CorrectListener implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
